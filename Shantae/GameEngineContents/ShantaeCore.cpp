99dc64dfa3f8453cc39d2c1b20245fd8c174b76c
엄태건 <umtimos@naver.com>
2023-03-03 16:01:17 +0900

    .

--- Shantae/GameEngineContents/ShantaeCore.cpp
+++ Shantae/GameEngineContents/ShantaeCore.cpp
@@ -98,7 +98,7 @@ void ShantaeCore::Start()
 	CreateLevel<TestRoom2>("TestRoom2");
 
 // ------------------------------- 레벨 선택 -------------------------------
-	ChangeLevel("TestLevel");
+	ChangeLevel("Opening");
 }
 
 void ShantaeCore::Update()
